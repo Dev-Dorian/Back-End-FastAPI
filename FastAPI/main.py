33f3c95fb8b6383b3c13a4dcaeab1ea852e2cdde
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-10-23 18:25:47 -0400

    images upload

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -4,12 +4,14 @@
 
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Url local: http://127.0.0.1:8000
 
